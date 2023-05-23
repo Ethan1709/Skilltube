f6a71032a128c2cf946e7759faef7d6184b1738c
Ethan1709 <benyayerethan@gmail.com>
2023-05-23 15:48:56 +0200

    adding homepage and video playing pages

--- skilltube/main/views.py
+++ skilltube/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(response):
+    return HttpResponse("<h1>Homepage skilltube view</h1>")
+
+def video(response):
+    return  HttpResponse("<h1>video playing page<h1>")
