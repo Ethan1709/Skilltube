3769ea3ea808f496506195d34c86cf8bd584c556
Ethan1709 <benyayerethan@gmail.com>
2023-05-31 19:04:11 +0200

    adding thumbnails

--- skilltube_backend/main/models.py
+++ skilltube_backend/main/models.py
@@ -7,6 +7,7 @@ class Video(models.Model):
     caption = models.CharField(max_length=255)
     video_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     video = models.FileField(upload_to='main/%y')
+    thumbnail = models.ImageField(null=True, blank=True, upload_to='images/')
 
 
     def __str__(self):
