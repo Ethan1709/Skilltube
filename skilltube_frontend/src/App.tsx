4f39b56d24181e1c68e2d2ef5cc325a6224ffe4c
simonrichard-dev <simon.insa@gmail.com>
2023-06-07 15:29:13 +0200

    add skilltube_frontend/ initial files

---
+++ skilltube_frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Skilltube</h1>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/wJ7FLJcGNCs" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+    </div>
+  );
+}
+
+export default App;
