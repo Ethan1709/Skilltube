4036fe15f964de5139dee996e9f7cdf958387960
Ethan1709 <benyayerethan@gmail.com>
2023-06-11 12:59:09 +0200

    adding login system

--- skilltube_backend/main/urls.py
+++ skilltube_backend/main/urls.py
@@ -4,9 +4,8 @@ from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('login', views.user_login, name='login'),
-    path('search', views.search, name='search'),
+    path('login', views.login_view, name='login'),
     path('registration/', views.signup, name='registration'),
-    path('profile/', views.profile, name='profile'),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path('search', views.search, name='search'),
 ]
--- skilltube_backend/main/views.py
+++ skilltube_backend/main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib.auth import login, authenticate
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -20,14 +21,12 @@ def video_list(request):
         videos = Video.objects.all()
         serializer = VideoSerializer(videos, many=True)
         return Response(serializer.data)
-    
+
+
 def index(request):
     video = Video.objects.all()
     return render(request, 'index.html',{'video':video})
 
-
-def user_login(request):
-    return render(request, 'login.html')
 
 def search(request):
     query = request.GET.get('caption', '')
@@ -40,19 +39,34 @@ def search(request):
     
     return render(request, 'search_results.html', context)
 
+
 def signup(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Hi {username}, your account has been successfully created')
-            return redirect('/')
+            success_message = f'Hi {username}, your account has been successfully created.'
+            return redirect(reverse('home') + '?success_message=' + success_message)
     else:
         form = UserRegisterForm()
 
     return render(request, 'registration.html', {'form':form})
 
-@login_required()
-def profile(request):
-    return render(request, 'users/profile.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            success_message = f"Welcome back, {user.username}!"
+            return redirect(reverse('home') + f'?success_message={success_message}')
+
+        else:
+            error_message = "Invalid password or username"
+            return redirect(reverse('login') + f'?error_message={error_message}')
+
+    return render(request, 'login.html')
