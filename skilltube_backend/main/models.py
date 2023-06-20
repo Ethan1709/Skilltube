b257fdbc5ca742da04db02781480a5336f021ec8
simonrichard-dev <simon.insa@gmail.com>
2023-06-20 16:29:20 +0200

    change uppercases in CATEGORY_CHOICES

--- skilltube_backend/main/models.py
+++ skilltube_backend/main/models.py
@@ -6,24 +6,24 @@ from datetime import datetime
 
 class Video(models.Model):
     CATEGORY_CHOICES = [
-        ('noob', 'Noob'),
-        ('beginner', 'Beginner'),
-        ('intermediate', 'Intermediate'),
-        ('advanced', 'Advanced'),
-        ('masterclass', 'Masterclass'),
+        ('Noob', 'Noob'),
+        ('Beginner', 'Beginner'),
+        ('Intermediate', 'Intermediate'),
+        ('Advanced', 'Advanced'),
+        ('Masterclass', 'Masterclass'),
     ]
     caption = models.CharField(max_length=255)
     video_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     video = models.FileField(upload_to='main/%y')
     thumbnail = models.ImageField(null=True, blank=True, upload_to='images/')
     created_at = models.DateTimeField(default=datetime.now)
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='beginner')
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='Beginner')
 
     def __str__(self):
         return self.caption
     
     def save(self, *args, **kwargs):
         if not self.category:
-            self.category = 'beginner'
+            self.category = 'Beginner'
         super().save(*args, **kwargs)
     
