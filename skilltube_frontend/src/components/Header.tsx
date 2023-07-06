8910cc94465593471ee1468483f869733b8fa026
Ethan1709 <113636658+Ethan1709@users.noreply.github.com>
2023-07-06 10:19:29 +0200

    Delete skilltube_frontend directory

--- skilltube_frontend/src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-export const Header = () => {
-    return (
-        <div className="header">
-            <h1>Skilltube</h1>
-            <h3>Choose the red pill or the blue pill</h3>
-        </div>
-    )
-}
